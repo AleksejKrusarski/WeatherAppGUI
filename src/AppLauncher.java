d81334aada4c17230966bbeb4f77a673f167510a
Aleksej Krusharski <aleksejkrusarski@hotmail.com>
2023-10-27 16:48:59 +0200

    Finall check

--- src/AppLauncher.java
+++ src/AppLauncher.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class AppLauncher {
+import javax.swing.*;
+
+public class AppLauncher {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                //display our weather app GUI
+                new WeatherAppGui().setVisible(true);
+                //test example
+                    //System.out.println(WeatherAppBackend.getLocationData("Tokyo"));
+                    //System.out.println(WeatherAppBackend.getCurrentTime());
+            }
+        });
+    }
 }
